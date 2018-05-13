cde2db6a87fa257aad610c7eaec72cb70658d82e
TUtso <utsombstu@gmail.com>
2018-05-14 01:28:23 +0600

    sms sending

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const ejs = require('ejs')
-const Nexmo = require('nexmo')
-const Socketio = require('socket.io')
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const Nexmo = require("nexmo");
+const Socketio = require("socket.io");
 
-const app = express()
+const nexmo = new Nexmo(
+  {
+    apiKey: "57aea764",
+    apiSecret: "3oCT9pnn1qYJfmC9"
+  },
+  {
+    debug: true
+  }
+);
 
-app.set('view engine', 'html')
-app.engine('html', ejs.renderFile)
+const app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.set("view engine", "html");
+app.engine("html", ejs.renderFile);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + "/public"));
 
-const port = 3000
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  const number = req.body.number;
+  const text = req.body.text;
+
+  nexmo.message.sendSms(
+    "NEXMO",
+    number,
+    text,
+    { type: "unicode" },
+    (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.dir(responseData);
+      }
+    }
+  );
+}); 
+
+const port = 3000;
 
 app.listen(port, () => {
-    console.log(`server started on port ${port}`);
-})
+  console.log(`server started on port ${port}`);
+});
